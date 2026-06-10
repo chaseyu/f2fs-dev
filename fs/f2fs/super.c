bcd933236deee25a87d382d70705a1577f4f2793
Bart Van Assche <bvanassche@acm.org>
2026-06-10 12:34:20 -0700

    f2fs: Run f2fs_write_end_io() asynchronously
    The bio_for_each_segment_all() loop can take more than 10 ms for a large
    bio on an ARM little core. This is too much for interrupt context. Hence
    perform the write bio completion work asynchronously if a bio is large and
    if f2fs_write_end_io() is called from atomic context. This patch reduces
    the time spent in f2fs_write_end_io() from about 10 ms to about 150
    microseconds on an Arm Cortex-A520 core.
    
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -5068,6 +5068,11 @@ try_onemore:
 
 	sb->s_fs_info = sbi;
 	sbi->raw_super = raw_super;
+	/*
+	 * SZ_16K restricts the time spent on completing writes to about 150
+	 * microseconds on an Arm Cortex-A520 core.
+	 */
+	sbi->max_atc_write_bio_size = SZ_16K;
 
 	INIT_WORK(&sbi->s_error_work, f2fs_record_error_work);
 	memcpy(sbi->errors, raw_super->s_errors, MAX_F2FS_ERRORS);
