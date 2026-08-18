304ec0da2ef581c89b769525e040e24e1cc8e060
Chao Yu <chao@kernel.org>
2026-08-18 11:54:37 +0000

    f2fs: cache: introduce tracepoints
    This patch introduces ftrace tracepoints to observe and profile metadata
    cache operations.
    
    Tracepoints added:
    - trace_f2fs_cache_set_dirty: Traces when a cached entry is marked dirty.
    - trace_f2fs_write_cache: Traces when an individual cached block (meta or node)
      is submitted for writeback.
    - trace_f2fs_write_caches: Traces batch writeback and sync sessions for metadata
      cache instances.
    
    Signed-off-by: Chao Yu <chao@kernel.org>

--- fs/f2fs/cache.c
+++ fs/f2fs/cache.c
@@ -18,6 +18,7 @@
 #include "f2fs.h"
 #include "cache.h"
 #include "node.h"
+#include <trace/events/f2fs.h>
 #include "segment.h"
 
 void f2fs_cache_wait_writeback_cond(struct f2fs_cached_block *entry,
@@ -80,6 +81,7 @@ bool f2fs_mark_cache_dirty(struct f2fs_cached_block *entry)
 		enum count_type type = IS_META_CACHE(cache) ?
 				F2FS_DIRTY_META : F2FS_DIRTY_NODES;
 
+		trace_f2fs_cache_set_dirty(entry, IS_META_CACHE(cache) ? META : NODE);
 		f2fs_cache_update_tag(entry, 0, F2FS_CACHE_TAG_DIRTY);
 		inc_page_count(cache->sbi, type);
 		return true;
